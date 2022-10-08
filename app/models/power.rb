8b484800cb305954c7af364b7552d80c23ac2db0
Peter Kimani <peterjajakim@gmail.com>
2022-10-08 16:43:27 +0300

    CREATED VALIDATIONS IN POWER MODEL

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,8 @@
 class Power < ApplicationRecord
     has_many :heros, through: :hero_powers
+    validates :description, {
+        presence: true,
+        length: {
+            minimum: 20
+        }
 end
