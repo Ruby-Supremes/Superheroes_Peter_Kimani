d1810aa82d290421de7375eede9859ed5323c074
Peter Kimani <peterjajakim@gmail.com>
2022-10-08 17:23:03 +0300

    CREATED HEROES GET ROUTE

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,6 @@
 class HeroesController < ApplicationController
+    def index
+        heroes = Hero.all
+        render json: heroes, include: ['id','name','super_name']
+    end
 end
--- app/serializers/heroes_serializer.rb
+++ app/serializers/heroes_serializer.rb
@@ -1,3 +1,3 @@
 class HeroesSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :super_name
 end
