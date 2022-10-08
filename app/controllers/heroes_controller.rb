b62ff48406e274236badd92074cd901635fa3133
Peter Kimani <peterjajakim@gmail.com>
2022-10-08 17:47:13 +0300

    CREATED AN INDEX AND SHOW ROUTE FOR HEROES

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -3,4 +3,8 @@ class HeroesController < ApplicationController
         heroes = Hero.all
         render json: heroes, include: ['id','name','super_name']
     end
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero
+    end
 end
