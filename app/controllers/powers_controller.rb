072e94ab7c6b7f65b0667094b6ac9b618c0d4426
Peter Kimani <peterjajakim@gmail.com>
2022-10-08 18:47:37 +0300

    CREATED A PATCH ROUTE FOR POWERS

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -4,8 +4,20 @@ class PowersController < ApplicationController
         render json: powers
     end
     def show
-        power = Power.find(params[:id])
-        render json: power
+        @power = Power.find(params[:id])
+        render json: @power
     end
+    def update
+        if @power.update(power_params)
+          render json: @power
+        else
+          render json: @power.errors, status: :unprocessable_entity
+        end
+    end
+    private
+    def power_params
+        params.permit(:description)
+    end
+
     
 end
