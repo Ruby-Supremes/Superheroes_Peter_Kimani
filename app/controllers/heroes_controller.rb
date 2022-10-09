7801ff213eacd6a4357331ccbb0419535abcf621
Peter Kimani <peterjajakim@gmail.com>
2022-10-09 16:02:34 +0300

    MADE CODE MORE ORGANISED

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,15 +1,21 @@
 class HeroesController < ApplicationController
+
 rescue_from ActiveRecord::RecordNotFound, with: :hero_not_found
+
     def index
         heroes = Hero.all
         render json: heroes, include: ['id','name','super_name']
     end
+
     def show
         hero = Hero.find(params[:id])
         render json: hero
     end
+
     private
+    
     def hero_not_found
         render json: { error: "Hero not found" }, status: 404
     end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,29 +1,37 @@
 class PowersController < ApplicationController
+
 rescue_from ActiveRecord::RecordNotFound, with: :power_not_found
 rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+
     def index
         powers = Power.all
         render json: powers
     end
+
     def show
-        @power = Power.find(params[:id])
-        render json: @power
+        power = Power.find(params[:id])
+        render json: power
     end
+
     def update
         power = find_power
         power.update(power_params)
         render json: power
     end
+
     private
     def power_params
         params.permit(:description)
     end
+
     def power_not_found
         render json: { error: "Power not found" }, status: 404
     end
+    
     def validation_error
         render json: { error:  ["validation errors"]}, status: 422
     end
+    
     def find_power
         Power.find(params[:id])
     end
