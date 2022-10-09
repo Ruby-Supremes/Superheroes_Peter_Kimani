cf2a0b4e249705e757a2386abdba586c17b20617
Peter Kimani <peterjajakim@gmail.com>
2022-10-09 20:09:21 +0300

    CORRECTED HERO_POWERS CONTROLLER

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -9,6 +9,7 @@ rescue_from ActiveRecord::RecordInvalid, with: :validation_error
       def hero_powers_params
         params.permit(:strength, :power_id, :hero_id)
       end
+      
       def validation_error
         render json: { error:  ["validation errors"]}, status: 422
       end
