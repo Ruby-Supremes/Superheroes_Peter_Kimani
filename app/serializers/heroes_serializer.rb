7c5a211aaa0460c25c7b8f3b3e36450b5bb09136
Peter Kimani <peterjajakim@gmail.com>
2022-10-09 14:57:41 +0300

    CORRECTED HERO AND POWER SERIALIZERS

--- app/serializers/heroes_serializer.rb
+++
@@ -1,4 +0,0 @@
-class HeroesSerializer < ActiveModel::Serializer
-  attributes :id, :name, :super_name
-  has_many :powers
-end
--- app/serializers/powers_serializer.rb
+++
@@ -1,3 +0,0 @@
-class PowersSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
-end
